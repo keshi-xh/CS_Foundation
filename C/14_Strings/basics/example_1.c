4f41d9d28c5f5b15427e8ea877099c3100b1a946
keshi-xh <keshangeethanjana@gmail.com>
2025-06-22 18:42:48 +0530

    Improve input handling in example programs by using fgets and limiting input size

--- C/14_Strings/basics/example_1.c
+++ C/14_Strings/basics/example_1.c
@@ -2,8 +2,12 @@
 
 int main() {
     char city[30];
+
     printf("Enter your city: ");
-    scanf("%s", city);
+
+    scanf("%29s", city); // Limit input to 29 characters
+
     printf("You live in: %s\n", city);
+
     return 0;
 }
